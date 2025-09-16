de2cf2d249dfcc2e5b68ffb7e85cdccca70ccc0a
Dhruv <dhruv00nv@gmail.com>
2025-09-16 10:06:20 +0530

    final commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -87,7 +87,3 @@ module.exports.getCars = async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
-
-module.exports.msg = (req, res) => {
-  res.send("This is user message");
-};
