8d480ee547f62772dfb742cb4ffe8cdf246b9f53
Dhruv <dhruv00nv@gmail.com>
2025-09-16 10:10:46 +0530

    final commit

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,7 +4,6 @@ const {
   loginUser,
   getUserData,
   getCars,
-  msg,
 } = require("../controllers/userController.js");
 const { protect } = require("../middleware/auth.js");
 const userRouter = express.Router();
@@ -13,6 +12,5 @@ userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUserData);
 userRouter.get("/cars", getCars);
-userRouter.get("/msg", msg);
 
 module.exports = userRouter;
